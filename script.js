10632703e689c6365c95348c92ec18c73ea0cd9b
Weslley souza <120567284+WeslleyNsou@users.noreply.github.com>
2024-11-21 09:31:35 -0300

    projeto concluido

--- script.js
+++ script.js
@@ -17,8 +17,34 @@ function checkInputs(){
     const passwordValue = password.value;
     const passwordconfimationValue = passwordconfimation.value;
 
-    if(username === ""){
+    if(usernameValue === ""){
         setErrorFor(username, "O nome de usuario e obrigatório.");
+    }else{
+        setSuccessFor(username)
+    }
+
+    if(emailValue === ""){
+        setErrorFor(email, "Email obrigatorio!")
+    }else if(!checkEmail (emailValue)){
+        setErrorFor(email, "Digite um email valido!")
+    }else{
+        setSuccessFor(email)
+    }
+
+    if(passwordValue === ""){
+        setErrorFor(password, "Senha obrigatoria!")
+    }else if(passwordValue.length < 7){
+        setErrorFor(password, "A senha deve conter 8 digitos!")
+    }else{
+        setSuccessFor(password)
+    }
+
+    if(passwordconfimationValue === ""){
+        setErrorFor(passwordconfimation, "Confirmação de senha obrigatoria!")
+    }else if(passwordconfimationValue !== passwordValue){
+        setErrorFor(passwordconfimation, "Senhas não conferem!")
+    }else{
+        setSuccessFor(passwordconfimation)
     }
 }
 
@@ -39,3 +65,9 @@ function setSuccessFor(input){
     //adiciona a classe de sucesso
     formControl.className = "form-control sucess";
 }
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  }
